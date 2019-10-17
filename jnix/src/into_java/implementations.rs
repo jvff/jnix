63f77c8a0fe89ccfd0771d88d3eb478852f29664
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-10-17 15:17:30 +0000

    Implement `IntoJava` for `bool`

--- jnix/src/into_java/implementations.rs
+++ jnix/src/into_java/implementations.rs
@@ -1,5 +1,23 @@
 use crate::IntoJava;
-use jni::{objects::AutoLocal, JNIEnv};
+use jni::{
+    objects::AutoLocal,
+    sys::{jboolean, JNI_FALSE, JNI_TRUE},
+    JNIEnv,
+};
+
+impl<'borrow, 'env: 'borrow> IntoJava<'borrow, 'env> for bool {
+    const JNI_SIGNATURE: &'static str = "Z";
+
+    type JavaType = jboolean;
+
+    fn into_java(self, _: &'borrow JNIEnv<'env>) -> Self::JavaType {
+        if self {
+            JNI_TRUE
+        } else {
+            JNI_FALSE
+        }
+    }
+}
 
 impl<'borrow, 'env: 'borrow> IntoJava<'borrow, 'env> for String {
     const JNI_SIGNATURE: &'static str = "Ljava/lang/String;";
