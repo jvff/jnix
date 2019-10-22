fe2d0df16b8ef08f8b359b6adb21405032a8850e
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-10-22 18:38:23 +0000

    Add `jni_signature` method the `IntoJava` trait

--- jnix/src/into_java/mod.rs
+++ jnix/src/into_java/mod.rs
@@ -8,4 +8,8 @@ pub trait IntoJava<'borrow, 'env: 'borrow> {
     type JavaType;
 
     fn into_java(self, env: &'borrow JNIEnv<'env>) -> Self::JavaType;
+
+    fn jni_signature(&self) -> &'static str {
+        Self::JNI_SIGNATURE
+    }
 }
