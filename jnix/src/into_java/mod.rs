1a5a60c28d3441f60734eef5da7da786617356c1
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-10-24 17:37:28 +0000

    Add `AsJValue<'env>` constraint to `JavaType`

--- jnix/src/into_java/mod.rs
+++ jnix/src/into_java/mod.rs
@@ -1,11 +1,12 @@
 mod implementations;
 
+use crate::AsJValue;
 use jni::JNIEnv;
 
 pub trait IntoJava<'borrow, 'env: 'borrow> {
     const JNI_SIGNATURE: &'static str;
 
-    type JavaType;
+    type JavaType: AsJValue<'env>;
 
     fn into_java(self, env: &'borrow JNIEnv<'env>) -> Self::JavaType;
 
