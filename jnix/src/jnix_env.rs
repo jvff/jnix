6af0789692f3d28ae1e44e57de89338fca866491
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2019-10-27 23:13:29 +0000

    Allow preloading classes into cache

--- jnix/src/jnix_env.rs
+++ jnix/src/jnix_env.rs
@@ -43,7 +43,18 @@ impl<'env> JnixEnv<'env> {
         }
     }
 
-    pub fn load_class(&self, class_name: impl AsRef<str>) -> GlobalRef {
+    pub fn preload_classes(&self, class_names: impl IntoIterator<Item = impl Into<String>>) {
+        let mut cache = CLASS_CACHE.lock();
+
+        for class_name in class_names {
+            let class_name = class_name.into();
+            let class = self.load_class(&class_name);
+
+            cache.insert(class_name, class);
+        }
+    }
+
+    fn load_class(&self, class_name: impl AsRef<str>) -> GlobalRef {
         let class_name = class_name.as_ref();
         let local_ref = self
             .env
